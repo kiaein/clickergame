6c96d7414eecb8f998b82c6ac2b5e1d297002e8f
kiaein <39696732+kiaein@users.noreply.github.com>
2021-02-13 04:36:01 -0400

    menu, modal and delete data option added

--- js/main.js
+++ js/main.js
@@ -4,13 +4,11 @@ class User{
     }
 }
 
-var temp;
 var currentUser;
 
 fetchUser();
 
 document.getElementById("clickButton").addEventListener("click", addClick);
-
 
 function addClick() {
     currentUser.clicks++;
@@ -23,7 +21,7 @@ function saveUser(){
 }
 
 function fetchUser(){
-    temp = JSON.parse(window.localStorage.getItem('user'));
+    var temp = JSON.parse(window.localStorage.getItem('user'));
     if(temp == null){
         currentUser= new User(0);
     }
@@ -35,4 +33,46 @@ function fetchUser(){
 
 function updateUI(){
     document.getElementById("clickNbText").innerHTML = currentUser.clicks;
+}
+
+//modal behavior 
+
+var modal = document.getElementById("mainModal");
+var closemodalbtn = document.getElementById("closemodalbutton");
+var okButton = document.getElementById("okmodalbutton");
+var cancelButton = document.getElementById("cancelmodalbutton");
+
+closemodalbtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+okButton.onclick = function() {
+    if(okButton.innerHTML=="delete it"){
+        deleteUser();
+    }
+}
+
+cancelButton.onclick = function() {
+    modal.style.display = "none";
+}
+
+//delete user
+
+var deleteinfobutton = document.getElementById("deleteinfobutton");
+
+deleteinfobutton.onclick = function() {
+    document.getElementById("modal-text").innerHTML= "are you sure you want to delete your information?<br>this will reset all of your data, including your clicks";
+    modal.style.display = "block";
+    okButton.innerHTML= "delete it";
+    cancelButton.innerHTML="no what the fuck";
+}
+
+function deleteUser(){
+    window.localStorage.removeItem('user');    
 }
