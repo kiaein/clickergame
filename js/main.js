0a3ce0b6edd21c7de7f9e123e3a67a3ecb392b20
kiaein <39696732+kiaein@users.noreply.github.com>
2021-02-13 08:18:57 -0400

    added shop, autoclick and clicklevel

--- js/main.js
+++ js/main.js
@@ -1,41 +1,98 @@
+// The One That Holds Everything //
+
 class User{
-    constructor(clicks){
+    constructor(clicks, autoClickLevel, clickLevel){
         this.clicks = clicks;
+        this.autoClickLevel = autoClickLevel;
+        this.clickLevel = clickLevel;
     }
 }
+
+// Gets all that juicy user Stuff //
 
 var currentUser;
 
 fetchUser();
 
-document.getElementById("clickBtn").addEventListener("click", addClick);
-
-function addClick() {
-    currentUser.clicks++;
-    updateUI();
-    saveUser();
-}
-
-function saveUser(){
-    window.localStorage.setItem('user', JSON.stringify(currentUser));
-}
-
 function fetchUser(){
     var temp = JSON.parse(window.localStorage.getItem('user'));
     if(temp == null){
-        currentUser= new User(0);
+        currentUser= new User(0,-1,1);
     }
     else{
-        currentUser = new User(temp.clicks);
-        updateUI();
+        currentUser = new User(temp.clicks, temp.autoClickLevel, temp.clickLevel);
+        //TODO: same thing, change this 
+        if(currentUser.clicks >= 10 || currentUser.autoClickLevel >= 0){
+            unlockAutoClick();
+        }
+        updateClick();
     }
 }
 
-function updateUI(){
+// clicking Stuff!! //
+
+function addClick(modifier=1) {
+    currentUser.clicks = currentUser.clicks + modifier;
+    updateClick();
+    
+    //TODO: change this to smth more or less challenging? this is a clicking game
+    if(currentUser.clicks == 10){
+        unlockAutoClick();
+    }
+}
+
+function updateClick(){
     document.getElementById("clickNbText").innerHTML = currentUser.clicks;
 }
 
-//modal behavior 
+//TODO: saves progress every second, change timer when things actually get Big
+setInterval(function() { 
+    window.localStorage.setItem('user', JSON.stringify(currentUser));
+}, 1000);
+
+// events ! //
+
+function unlockAutoClick(){
+    if(currentUser.autoClickLevel > 0){
+        document.getElementById("autoClickBtn").innerHTML = "auto click: " + currentUser.autoClickLevel;
+        updateAutoClick();
+    }
+    else{
+        currentUser.autoClickLevel = 0;
+    }
+    document.getElementById("shop").style.visibility='visible';
+    document.getElementById("autoClickBtn").style.visibility='visible';
+}
+
+// shop buttons //
+
+document.getElementById("autoClickBtn").addEventListener("click", buyAutoClick);
+
+function buyAutoClick() {
+    var price = (currentUser.autoClickLevel + 1)*10;
+    if(currentUser.clicks >= price ){
+        currentUser.clicks = currentUser.clicks-price;
+        currentUser.autoClickLevel++;
+        document.getElementById("autoClickBtn").innerHTML = "auto click: " + currentUser.autoClickLevel;
+        updateClick();
+        updateAutoClick();
+    }
+}
+
+// item effects //
+var autoClickTimer; 
+
+function autoClick(){
+    var timer = 2000; //clicks every 2 seconds for you
+    autoClickTimer = setInterval(function(){addClick(currentUser.autoClickLevel)}, timer)
+}
+
+function updateAutoClick(){
+    clearTimeout(autoClickTimer);
+    autoClick();
+}
+
+// modal behavior //
 
 var modal = document.getElementById("mainModal");
 var closeModalBtn = document.getElementById("closeModalBtn");
@@ -63,13 +120,13 @@ cancelBtnModal.onclick = function() {
     modal.style.display = "none";
 }
 
-//delete user
+// delete user //
 
 var deleteinfobtn = document.getElementById("deleteInfoBtn");
 
 deleteinfobtn.onclick = function() {
-    modalText.innerHTML= "are you sure you want to delete your information?<br>this will reset all of your data, including your clicks";
     modal.style.display = "block";
+    modalText.innerHTML= "are you sure you want to delete your information?<br>this will reset all of your data, including your clicks";
     okBtnModal.innerHTML= "delete it";
     cancelBtnModal.innerHTML="no what the fuck";
 }
